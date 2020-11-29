c1c4c0c5609a06e297088775d62014d62be345d7
Alex <alexmogwaiii@gmail.com>
2020-11-29 22:38:32 +0200

    added menu bar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,37 @@
 'use strict';
 
 const lang = document.querySelector('.nav__select-lang');
+const langList = document.querySelector('.nav__lang-list');
+const selectedLang = document.querySelector('.nav__selected-lang');
 
 lang.addEventListener('click', function() {
-  const langList = document.querySelector('.nav__lang-list');
   const langArrow = document.querySelector('.nav__arrow');
 
-  if (langList.classList.contains('nav__lang-list--active')) {
-    langList.classList.remove('nav__lang-list--active');
-    langArrow.classList.remove('nav__arrow--active');
-  } else {
-    langList.classList.add('nav__lang-list--active');
-    langArrow.classList.add('nav__arrow--active');
+  langList.classList.toggle('nav__lang-list--active');
+
+  langArrow.classList.toggle('nav__arrow--active');
+});
+
+langList.addEventListener('click', function(e) {
+  if (e.target.classList.contains('nav__lang-item--ua')) {
+    selectedLang.textContent = 'UA';
+  } else if (e.target.classList.contains('nav__lang-item--eng')) {
+    selectedLang.textContent = 'ENG';
+  }
+});
+
+//  fade in menu
+
+const hamburgerMenu = document.querySelector('.nav__menu');
+
+hamburgerMenu.addEventListener('click', function(e) {
+  const headerMenu = document.querySelector('.header__menu');
+  const hamburger = document.querySelector('.nav__hamburger');
+
+  headerMenu.classList.toggle('header__menu--active');
+  hamburger.classList.toggle('nav__hamburger--active');
+
+  if (document.documentElement.clientWidth <= 768) {
+    lang.classList.toggle('nav__select-lang--active');
   }
 });
