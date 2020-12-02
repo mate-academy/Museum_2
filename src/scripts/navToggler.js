ed4dd1c69a33cd600626cae4a29928610b6cebf3
ShyianO <ol.shyian@gmacil.com>
2020-12-03 00:46:14 +0200

    News, scheduler, footer components were added, modifiers were renamed

--- src/scripts/navToggler.js
+++ src/scripts/navToggler.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.nav__burger-menu');
+const navButton = document.querySelector('.nav__btn');
+
+export const navToggler = navButton.addEventListener('click', () => {
+  navButton.classList.toggle('active')
+  burgerMenu.classList.toggle('show');
+});
