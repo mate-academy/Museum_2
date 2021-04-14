e6e92f3db5397add9c026c1a297e34e6c3c9c102
”Ivan <ivasylenko@ukr.net>
2021-04-14 17:48:11 +0300

    Header Nav Actual

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const nav = document.querySelector('.nav');
+const icon = document.querySelector('.header__menu-icon');
+const mainImg = document.querySelector('.header__img');
+const toggler = document.querySelector('#toggler-nav');
+
+toggler.addEventListener('click', () => {
+  nav.classList.toggle('nav--active');
+  icon.classList.toggle('header__menu-icon--close');
+  mainImg.classList.toggle('header__img--hidden');
+});
