4961689f29a449e12b3a659245fa5102bc8f9590
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-04-16 17:02:30 +0300

    header is added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const sidebar = document.querySelector('.headline');
+const toggler = document.querySelector('#nav-toggler');
+const label = document.querySelector('.nav__toggler-label');
+
+toggler.addEventListener('click', () => {
+  sidebar.classList.toggle('headline--active');
+  label.classList.toggle('nav__toggler--close');
+});
