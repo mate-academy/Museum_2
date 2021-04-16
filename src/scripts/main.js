d7fc0053338ea3c3f23062d436386cf44312be5a
”Ivan <ivasylenko@ukr.net>
2021-04-16 17:32:10 +0300

    Added news scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,3 +13,49 @@ const menuToggler = () => {
 
 toggler.addEventListener('click', menuToggler);
 nav.addEventListener('click', menuToggler);
+
+const btn1 = document.querySelector('#news-btn--1');
+const btn2 = document.querySelector('#news-btn--2');
+const btn3 = document.querySelector('#news-btn--3');
+
+btn1.addEventListener('click', () => {
+  document.getElementById('news__block-scroll').className
+  = 'news__block-scroll news__block-scroll--1';
+
+  document.getElementById('news-btn--1').className
+  = 'news__block-toggler-btn--active';
+
+  document.getElementById('news-btn--2').className
+  = 'news__block-toggler-btn';
+
+  document.getElementById('news-btn--3').className
+  = 'news__block-toggler-btn';
+});
+
+btn2.addEventListener('click', () => {
+  document.getElementById('news__block-scroll').className
+  = 'news__block-scroll news__block-scroll--2';
+
+  document.getElementById('news-btn--1').className
+  = 'news__block-toggler-btn';
+
+  document.getElementById('news-btn--2').className
+  = 'news__block-toggler-btn--active';
+
+  document.getElementById('news-btn--3').className
+  = 'news__block-toggler-btn';
+});
+
+btn3.addEventListener('click', () => {
+  document.getElementById('news__block-scroll').className
+  = 'news__block-scroll news__block-scroll--3';
+
+  document.getElementById('news-btn--1').className
+  = 'news__block-toggler-btn';
+
+  document.getElementById('news-btn--2').className
+  = 'news__block-toggler-btn';
+
+  document.getElementById('news-btn--3').className
+  = 'news__block-toggler-btn--active';
+});
