73bbc14fc6e2311bf337aec5acd2211cecf892ac
tkachukione <tkachukione@gmail.com>
2021-07-19 16:03:32 +0300

    second

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const menu = document.querySelector('.header__manu-opener');
+const navigation = document.querySelector('.navigation');
+
+menu.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--active');
+  menu.classList.toggle('header__manu-opener--active');
+});
+
+window.addEventListener('scroll', () => {
+  navigation.classList.remove('navigation--active');
+  menu.classList.remove('header__manu-opener--active');
+});
