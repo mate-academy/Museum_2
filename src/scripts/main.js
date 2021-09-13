89b47658f39ff2d3ed8b3c2d3612990d4e7f2e17
Kate Kasuba <otroshko12@gmail.com>
2021-09-13 11:23:34 +0300

    Museum 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,27 @@ menuOpener.addEventListener('click', function() {
 menu.addEventListener('click', () => {
   body.classList.remove('page__body--with-menu');
 });
+
+const button = document.querySelector('.sign-up__button');
+const form = document.querySelector('.sign-up__form');
+
+form.addEventListener('change', () => {
+  button.classList.add('sigh-up__button--email');
+});
+
+const list = document.querySelector('.slide__list');
+const first = document.querySelector('.button--1');
+const second = document.querySelector('.button--2');
+const third = document.querySelector('.button--3');
+
+first.addEventListener('click', () => {
+  list.style.transform = `translate(0%)`;
+});
+
+second.addEventListener('click', () => {
+  list.style.transform = `translate(-100%)`;
+});
+
+third.addEventListener('click', () => {
+  list.style.transform = `translate(-200%)`;
+});
