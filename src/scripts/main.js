d1e8f3891f29b90e308b070cd12d74ad9701056a
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-10-28 12:48:49 +0300

    Implement desktop version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const menuIcon = document.querySelector('#menuIcon');
+const menu = document.querySelector('#menu');
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('hamburger--active');
+  menu.classList.toggle('page__menu--show');
+});
+
+const buyButtons = document.querySelectorAll('.btn');
+const alertBox = document.querySelector('#alert');
+const alertCloseBtn = document.querySelector('.alert__close-btn');
+
+buyButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    alertBox.classList.add('page__alert--show');
+  });
+});
+
+alertCloseBtn.addEventListener('click', () => {
+  alertBox.classList.remove('page__alert--show');
+});
