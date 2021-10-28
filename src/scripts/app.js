c38b246daa0820a6b9ad6a20f76f852695d82584
Ihor Poliakov <junglqq@gmail.com>
2021-10-28 22:35:55 +0300

    add

---
+++ src/scripts/app.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelector('.wrapper').classList.add('_loaded');
+});
+
+const iconMenu = document.querySelector(".icon-menu");
+const menuBody = document.querySelector(".menu__body");
+const body = document.querySelector("body");
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+iconMenu.addEventListener("click", function (e) {
+	iconMenu.classList.toggle("_active");
+	menuBody.classList.toggle("_active");
+	body.classList.toggle("_lock");
+})
+
+navLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		iconMenu.classList.remove('_active');
+		menuBody.classList.remove('_active');
+		body.classList.remove('_lock');
+	})
+});
