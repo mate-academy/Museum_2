ebc4c67fddd4029f9a0e6212dd51b3ad7cb41a09
Olya <olya.shyian.fed@gmail.com>
2021-10-31 18:40:16 +0200

    Museum-v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burgerMenu = document.querySelector('.menu');
+const headerBtn = document.querySelector('.header__btn');
+const navButton = document.querySelector('.btn-toggler');
+
+headerBtn.addEventListener('click', () => {
+  navButton.classList.toggle('header__btn-toggler--active');
+  burgerMenu.classList.toggle('menu--show');
+});
