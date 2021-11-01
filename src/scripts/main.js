f3b0bf9652f1c0ede49c45eeac220eefdf99dd81
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2021-11-01 18:54:08 +0200

    Museum 2 alpha v1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const menuButton = document.querySelector('.top-action__menu-btn');
+const menuPage = document.querySelector('.page__menu');
+const menuPageBody = document.querySelector('.page__body');
+let menuOpen = false;
+
+menuButton.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuButton.classList.add('top-action__menu-btn--open');
+    menuPage.classList.add('page__menu--open');
+    menuPageBody.classList.add('page__body--with-menu');
+    menuOpen = true;
+  } else {
+    menuButton.classList.remove('top-action__menu-btn--open');
+    menuPage.classList.remove('page__menu--open');
+    menuPageBody.classList.remove('page__body--with-menu');
+    menuOpen = false;
+  }
+});
