904e1d515c05c2c71f081405fcf18ddcaa9b9ed9
Ihor Poliakov <junglqq@gmail.com>
2021-11-02 18:56:44 +0200

    ff

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-	document.querySelector('.wrapper').classList.add('_loaded');
+  document.querySelector('.wrapper').classList.add('_loaded');
 });
 
 const iconMenu = document.querySelector(".icon-menu");
@@ -7,17 +7,26 @@ const menuBody = document.querySelector(".menu__body");
 const body = document.querySelector("body");
 
 const navLinks = document.querySelectorAll('.nav__link');
+const navButtons = document.querySelectorAll('.nav__button');
 
 iconMenu.addEventListener("click", function (e) {
-	iconMenu.classList.toggle("_active");
-	menuBody.classList.toggle("_active");
-	body.classList.toggle("_lock");
+  iconMenu.classList.toggle("_active");
+  menuBody.classList.toggle("_active");
+  body.classList.toggle("_lock");
 })
 
 navLinks.forEach((link) => {
-	link.addEventListener('click', () => {
-		iconMenu.classList.remove('_active');
-		menuBody.classList.remove('_active');
-		body.classList.remove('_lock');
-	})
+  link.addEventListener('click', () => {
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+    body.classList.remove('_lock');
+  })
+});
+
+navButtons.forEach((link) => {
+  link.addEventListener('click', () => {
+    iconMenu.classList.remove('_active');
+    menuBody.classList.remove('_active');
+    body.classList.remove('_lock');
+  })
 });
