12fa756233cbee83512564f3d8714d1a2629918d
Andriy Bulbuk <bulbuk.andriy@gmail.com>
2021-11-03 14:23:59 +0200

    Museum 2 alpha v1.1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,11 +7,21 @@ let menuOpen = false;
 
 menuButton.addEventListener('click', () => {
   if (!menuOpen) {
+    window.location.hash = '#menu';
+  } else {
+    window.location.hash = '#';
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
     menuButton.classList.add('top-action__menu-btn--open');
     menuPage.classList.add('page__menu--open');
     menuPageBody.classList.add('page__body--with-menu');
     menuOpen = true;
   } else {
+    document.body.classList.remove('page__body--with-menu');
     menuButton.classList.remove('top-action__menu-btn--open');
     menuPage.classList.remove('page__menu--open');
     menuPageBody.classList.remove('page__body--with-menu');
