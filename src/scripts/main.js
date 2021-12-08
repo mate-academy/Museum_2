867097cf9b422b4387fe5901ae3e66241c1b29b5
Nataliia Hrynyk <nataliiahrynyk2001@gmail.com>
2021-12-08 04:02:08 +0200

    init

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    for (const element of document.body.children) {
+      if (!element.classList.contains('menu')) {
+        setTimeout(() => {
+          element.classList.add('page__element-disabled');
+        }, 500);
+      }
+    }
+  } else {
+    for (const element of document.body.children) {
+      if (!element.classList.contains('menu')) {
+        element.classList.remove('page__element-disabled');
+      }
+    }
+  }
+});
