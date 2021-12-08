e72dc2d6a1741fdb03a22c3615fa6f7928a559d7
Yulia Koshkina <julyasav4uk@gmail.com>
2021-12-08 17:58:30 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+const form = document.querySelector('.subscription__form');
+const input = document.querySelectorAll('.subscription__form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  input.value = '';
+});
