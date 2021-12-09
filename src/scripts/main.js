1bcedb777733504f96de2611e37e80f4007dbacc
Nataliia Hrynyk <nataliiahrynyk2001@gmail.com>
2021-12-09 19:17:56 +0200

    landing is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,10 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    for (const element of document.body.children) {
-      if (!element.classList.contains('menu')) {
-        setTimeout(() => {
-          element.classList.add('page__element-disabled');
-        }, 500);
-      }
-    }
-  } else {
-    for (const element of document.body.children) {
-      if (!element.classList.contains('menu')) {
-        element.classList.remove('page__element-disabled');
-      }
-    }
-  }
+const form = document.getElementById('form');
+const input = document.querySelector('.digest__form-field');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  input.value = '';
 });
