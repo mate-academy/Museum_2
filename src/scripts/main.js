598c4a211f7e19866af1b1f0b1fe79b724fd71b4
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-17 23:59:04 +0200

    save 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const but = document.getElementById('link');
+const cross = document.getElementById('cross');
+
+but.addEventListener('click', function() {
+  document.getElementById('burger-menu').style.transform = 'translateY(0)';
+  document.getElementById('page').style.overflowY = 'hidden';
+});
+
+cross.addEventListener('click', function() {
+  document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
+  document.getElementById('page').style.overflowY = 'visible';
+});
