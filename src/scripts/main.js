72f4474f1a459ad8f8c738d5b210d6c57403d470
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-18 14:22:09 +0200

    final save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,12 @@
 const but = document.getElementById('link');
 const cross = document.getElementById('cross');
 
-but.addEventListener('click', function() {
+but.addEventListener('click', function () {
   document.getElementById('burger-menu').style.transform = 'translateY(0)';
   document.getElementById('page').style.overflowY = 'hidden';
 });
 
-cross.addEventListener('click', function() {
+cross.addEventListener('click', function () {
   document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
   document.getElementById('page').style.overflowY = 'visible';
 });
@@ -17,22 +17,36 @@ const actual = document.getElementById('actual-link');
 const coming = document.getElementById('coming-link');
 const news = document.getElementById('news-link');
 const menu = document.getElementById('menu-link');
+const slider = document.getElementById('slider');
 
-actual.addEventListener('click', function() {
+actual.addEventListener('click', function () {
   document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
   document.getElementById('page').style.overflowY = 'visible';
 });
 
-coming.addEventListener('click', function() {
+coming.addEventListener('click', function () {
   document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
   document.getElementById('page').style.overflowY = 'visible';
 });
 
-news.addEventListener('click', function() {
+news.addEventListener('click', function () {
   document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
   document.getElementById('page').style.overflowY = 'visible';
 });
 
-menu.addEventListener('click', function() {
+slider.addEventListener('click', function () {
+  document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
+  document.getElementById('page').style.overflowY = 'visible';
+});
+
+menu.addEventListener('click', function () {
   document.location.reload();
 });
+
+document.getElementById('sent').addEventListener('submit',
+  function (event) {
+    event.preventDefault();
+
+    document.getElementById('mail').value = '';
+  }
+);
