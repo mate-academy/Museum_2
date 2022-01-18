744dac985dbcbb3ab71211237c2e09afe9a5ccda
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-18 13:58:27 +0200

    save 5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,3 +12,27 @@ cross.addEventListener('click', function() {
   document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
   document.getElementById('page').style.overflowY = 'visible';
 });
+
+const actual = document.getElementById('actual-link');
+const coming = document.getElementById('coming-link');
+const news = document.getElementById('news-link');
+const menu = document.getElementById('menu-link');
+
+actual.addEventListener('click', function() {
+  document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
+  document.getElementById('page').style.overflowY = 'visible';
+});
+
+coming.addEventListener('click', function() {
+  document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
+  document.getElementById('page').style.overflowY = 'visible';
+});
+
+news.addEventListener('click', function() {
+  document.getElementById('burger-menu').style.transform = 'translateY(-100%)';
+  document.getElementById('page').style.overflowY = 'visible';
+});
+
+menu.addEventListener('click', function() {
+  document.location.reload();
+});
