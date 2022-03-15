192d6eb34c77b69d45489b720e415989e54c074e
VladSkoromnyi <anq028@gmail.com>
2022-03-15 20:34:46 +0200

    landing was finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.querySelector('#toTop');
+
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 100) {
+      btn.classList.add('show');
+    } else {
+      btn.classList.remove('show');
+    }
+  });
+});
