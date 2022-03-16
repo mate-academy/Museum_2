8f83db16702f832626b55171f35ec6f936540032
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-03-16 18:56:10 +0200

    Create web page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#drop-menu') {
+    document.body.classList.add('overflow');
+  } else {
+    document.body.classList.remove('overflow');
+  }
+});
+
+window.addEventListener('scroll', function() {
+  const scroll = this.document.querySelector('.top-scroll');
+
+  scroll.classList.toggle('top-scroll--off', window.scrollY < 300);
+});
