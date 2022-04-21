cbff527d177cb98970036b58563aca32682c04ac
Volodymyr <lesksv2000@gmail.com>
2022-04-21 20:49:14 +0300

    Museum 2 landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const burgerTrigger = header.querySelector('.header__burger');
+
+function toggleBurgerMenu() {
+  header.classList.toggle('header--active');
+}
+
+burgerTrigger.addEventListener('click', toggleBurgerMenu, true);
