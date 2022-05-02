c399581d9c43a13a7af607b20af4d72d798591fe
Volodymyr <lesksv2000@gmail.com>
2022-05-02 18:01:24 +0300

    museum final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,18 @@
 
 const header = document.querySelector('.header');
 const burgerTrigger = header.querySelector('.header__burger');
+const menuLinks = header.querySelectorAll('.menu__link ');
 
 function toggleBurgerMenu() {
   header.classList.toggle('header--active');
 }
 
-burgerTrigger.addEventListener('click', toggleBurgerMenu, true);
+function hideBurgerMenu() {
+  header.classList.remove('header--active');
+}
+
+burgerTrigger.addEventListener('click', toggleBurgerMenu, false);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', hideBurgerMenu, false);
+});
