d7b2d406754f8a5b65d73afa2b6bdefea0e471cd
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2022-05-16 20:19:25 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+      document.body.style.overflowY = "hidden";
+    } else {
+        document.body.style.overflowY = "auto";
+    }
+});
