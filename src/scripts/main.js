14b6d7eb72dcd610fea195ffd2d4ebef01fc12d1
DominoSl <artemenko.mik@gmail.com>
2022-05-30 13:35:43 +0300

    create NAMU landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document
+  .getElementsByClassName('footer__button-top')[0]
+  .addEventListener(
+    'click', () => window.scrollTo({
+      top: 0, behavior: 'smooth',
+    })
+  );
+
+const subscribeForm = document.getElementById('subscribe-form');
+
+subscribeForm.addEventListener('submit', ev => {
+  subscribeForm.reset();
+  ev.preventDefault();
+});
