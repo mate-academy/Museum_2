d0c25f5a9521cf728eed1fde6e1651d7af7229e2
Roman Marunkevich <vinnibea@gmail.com>
2022-06-02 01:14:44 +0300

    changed scrollbar, added links to icons in footer, added hover effect to top links, fixed backgorund

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,8 +5,14 @@ const menuHide = document.querySelector('.menu__container');
 const menuOpener = document.querySelector('.nav__icon--burger');
 const link = document.querySelectorAll('.menu__link');
 const home = document.querySelector('.namu__page');
+const logo = document.querySelector('.menu__logo');
 
 cross.addEventListener('click', function(e) {
+  menuHide.classList.remove('menu__container--open');
+  home.style.cssText = 'overflow-y: scroll';
+});
+
+logo.addEventListener('click', function(e) {
   menuHide.classList.remove('menu__container--open');
   home.style.cssText = 'overflow-y: scroll';
 });
@@ -20,3 +26,8 @@ link.forEach(e => e.addEventListener('click', function() {
   menuHide.classList.remove('menu__container--open');
   home.style.cssText = 'overflow-y: scroll';
 }));
+
+document.querySelector('.button--submit').addEventListener('click',
+  function(event) {
+    event.preventDefault();
+  }, true);
