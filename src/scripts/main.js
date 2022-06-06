c44c0e0d29979fa17276109aa12a6c927a0faaac
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-06-06 13:22:08 +0300

    subscribe done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,10 @@ document.getElementById('form').addEventListener('click', function(event) {
   event.preventDefault();
 });
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('page__body--with-menu');
+//   } else {
+//     document.body.classList.remove('page__body--with-menu');
+//   }
+// });
