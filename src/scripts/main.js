5af787b1847d64ff38ab23fec3eb82ac4cfa5d45
Oleksandr Kotliarov <de.fr09n@gmail.com>
2022-06-24 15:59:27 +0300

    home section was added for mobile and tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,4 +8,42 @@ burger.addEventListener('click', el => {
   burger.classList.toggle('active');
   nav.classList.toggle('active');
   language.classList.toggle('active');
+  document.body.classList.toggle('page__body--with-menu');
+});
+
+document.querySelectorAll('.nav__link').forEach(n => {
+  n.addEventListener('click', () => {
+    nav.classList.remove('active');
+    document.body.classList.remove('page__body--with-menu');
+    burger.classList.remove('active');
+    language.classList.remove('active');
+  });
+});
+
+document.querySelectorAll('.nav__btn').forEach(n => {
+  n.addEventListener('click', () => {
+    nav.classList.remove('active');
+    document.body.classList.remove('page__body--with-menu');
+    burger.classList.remove('active');
+    language.classList.remove('active');
+  });
+});
+
+document.querySelectorAll('.header__logo-link').forEach(n => {
+  n.addEventListener('click', () => {
+    nav.classList.remove('active');
+    document.body.classList.remove('page__body--with-menu');
+    burger.classList.remove('active');
+    language.classList.remove('active');
+  });
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+
+  window.location.href = '#home';
 });
