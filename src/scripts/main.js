2b02060b735b6d9d7df66684758b2395e0381893
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2022-07-13 09:58:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const form = document.querySelector('.subscribe__form');
+const email = document.querySelector('#email');
+const menu = document.querySelector('.menu__overflow');
+
+function scrollChange() {
+  if (window.location.hash === '#menu') {
+    body.style.overflow = 'hidden';
+    menu.scroll(0, 0);
+  }
+
+  if (window.location.hash !== '#menu') {
+    body.style.overflow = '';
+  }
+}
+
+form.addEventListener('submit', () => {
+  window.location.hash = '';
+  email.value = '';
+});
+
+window.onhashchange = scrollChange;
+
+scrollChange();
