3ba6680c94ac812fa9c6fc40b8d1e7284785274f
Nazar Kulchevych <gamebandit38@gmail.com>
2022-07-25 17:54:24 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const contactEmail = document.querySelector('.joining__email');
+const contactButton = document.querySelector('.joining__button');
+const contactForm = document.querySelector('.joining__form');
+
+contactButton.addEventListener('click', function(event) {
+  if (contactEmail.checkValidity()) {
+    event.preventDefault();
+    contactForm.reset();
+  }
+});
