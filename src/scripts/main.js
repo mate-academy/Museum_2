1f1230e58765433a3377663e294e66969d4a8a22
Ganna Krainiuk <anya.krainiuk@gmail.com>
2022-08-04 15:25:56 +0300

    form fixed 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,6 @@ window.addEventListener('hashchange', () => {
 const form = document.getElementById('formID');
 
 form.addEventListener('submit', (event) => {
-  // event.preventDefault();
-  event.reset();
+  event.preventDefault();
+  event.target.reset();
 });
