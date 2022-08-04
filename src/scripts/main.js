360c96b05723e3c11b9f42b9647fecfcbddd0fd5
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-08-04 12:27:37 +0300

    museum fix sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,14 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const email = document.getElementById('mail');
+
+email.addEventListener('input', function(event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('I am expecting an e-mail address!');
+    email.reportValidity();
+  } else {
+    email.setCustomValidity('');
+  }
+});
