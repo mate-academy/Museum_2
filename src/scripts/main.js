c2f5b97631fbcd68b1b85f6bbc1d8b33569eddc4
Vadls <vadim_tanasienko54541@ukr.net>
2022-08-04 11:58:36 +0300

    Header fixed, form's styles, hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,14 @@
 
 const form = document.getElementById('form');
 
-form.addEventListener('submit', function handleSubmit(event) {
-  event.preventDefault();
+form.addEventListener('submit', function handleSubmit() {
   form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
