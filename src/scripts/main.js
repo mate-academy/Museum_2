838365c9915ee829d5c538cc09387f95899e64a2
Oksana Kyryienko <kseniyals1609@gmail.com>
2022-08-05 20:29:00 +0300

    fixes after review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,16 @@
 'use strict';
 
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
-  }
-});
-
-const email = document.getElementById('mail');
-
-email.addEventListener('input', function(event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity('I am expecting an e-mail address!');
-    email.reportValidity();
-  } else {
-    email.setCustomValidity('');
   }
 });
