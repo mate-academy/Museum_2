8c3acbf5817369921e184d0c9e97edf51432d169
Killdex <kdcompany2018@gmail.com>
2022-08-30 20:46:38 +0300

    finished

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu');
   const langTopAction = document.querySelector('.top-action__lang');
 
+  const navLinks = document.querySelectorAll('.nav__link');
+
   burgerBtn.addEventListener('click', () => {
     menu.classList.toggle('menu--active');
     burgerBtn.classList.toggle('burger--active');
     langTopAction.classList.toggle('top-action__lang--active');
     document.body.classList.toggle('page__body--active');
+  });
+
+  navLinks.forEach(item => {
+    item.addEventListener('click', () => {
+      document.body.classList.remove('page__body--active');
+      menu.classList.remove('menu--active');
+      burgerBtn.classList.remove('burger--active');
+    });
   });
 });
