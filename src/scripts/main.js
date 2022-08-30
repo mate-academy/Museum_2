5e3ff2f10d49e98b4862f0a8c4552b09a825a3f1
Killdex <kdcompany2018@gmail.com>
2022-08-30 13:05:08 +0300

    created news block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+  const burgerBtn = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const langTopAction = document.querySelector('.top-action__lang');
+
+  burgerBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+    burgerBtn.classList.toggle('burger--active');
+    langTopAction.classList.toggle('top-action__lang--active');
+    document.body.classList.toggle('page__body--active');
+  });
+});
