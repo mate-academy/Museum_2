45148e8a424d9f6326c515145859f107a2da5d89
Vasiliy22 <hitretsukrnet@gmail.com>
2022-08-31 09:22:02 +0300

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('header--with-menu');
+  } else {
+    document.body.classList.remove('header--with-menu');
+  }
+});
+
+const myForm = document.querySelector('.my-form');
+
+const sendButton = document.getElementById('clicks');
+
+sendButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  myForm.reset();
+});
+
+myForm.addEventListener('clicks', sendButton);
