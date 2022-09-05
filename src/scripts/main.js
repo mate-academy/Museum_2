722056ea2f9a8e8e97ef408a32b84242f35476a9
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-09-05 21:08:52 +0300

    Created: menu and exhibition blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// const nav = document.querySelector('.menu');
+// const button = document.querySelector('.icon--menu');
+
+// button.addEventListener('click', function () {
+//   nav.classList.toggle('menu--open');
+// });
