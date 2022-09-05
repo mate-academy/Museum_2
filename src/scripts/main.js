af55fd97fc448f157b540dc86ab5d67b348a1c0c
Sasha Shevchenko <sasha.shevchenko763@gmail.com>
2022-09-05 14:55:32 +0300

    change language icon was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const elPin = document.querySelector('.icon--language-pin');
+const elLang = document.querySelector('.icon--language');
+
+const elPlinMenu = document.querySelector('.icon--language-pin-menu-phone');
+const elLngMenu = document.querySelector('.icon--language-menu-phone');
+
+elPin.addEventListener('click', function() {
+  elLang.textContent = elLang.textContent === 'UA' ? 'ENG' : 'UA';
+});
+
+elPlinMenu.addEventListener('click', function() {
+  elLngMenu.textContent = elLngMenu.textContent === 'UA' ? 'ENG' : 'UA';
+});
