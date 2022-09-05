aea984261c1926f2c5aa682cdb26b7f70317fb8d
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-09-05 11:18:30 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,7 @@ const button = document.getElementById('button');
 const inputField = document.getElementById('email');
 const form = document.getElementById('form');
 const regEx = /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/;
+const popUp = document.querySelector('.didgest__popup');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -12,5 +13,6 @@ form.addEventListener('submit', (event) => {
     button.classList.toggle('button--disabled');
     button.disabled = true;
     inputField.disabled = true;
+    popUp.classList.add('didgest__popup--active');
   }
 });
