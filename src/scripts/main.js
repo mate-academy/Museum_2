f49316ef28a96a6a31f8a4c7e3a73a07907c9d7d
Andriy Andrieiev <proph7000@gmail.com>
2022-09-05 12:09:38 +0300

    first try

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Overflow: hidden for .header__nav
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#sidebar') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// Cleared form fields after submit
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.reset();
+});
