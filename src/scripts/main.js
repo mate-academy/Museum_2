8b8915b6d13fabf75da37ed764b30753acd559c5
Anton Vaida <tohan1.ua@gmail.com>
2022-09-08 19:30:33 +0300

    add footer section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.sing_up__email');
+
+function handler(event) {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
+
+form.addEventListener('submit', handler);
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page--with-menu');
