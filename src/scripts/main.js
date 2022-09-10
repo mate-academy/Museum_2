bdf0bae81af48e5c1ebccb90be1453f26092c3f8
Sasha Kovalov <av.kov14@gmail.com>
2022-09-10 20:58:38 +0300

    add subscribe form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-on');
+  } else {
+    document.body.classList.remove('page__body--menu-on');
+  }
+});
+
+const form = document.getElementById('email-form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+  form.reset();
+});
