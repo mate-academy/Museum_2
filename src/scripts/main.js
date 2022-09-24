ec8b74fe363f5f76ae1a6ab2d19eb3c5be5b5d13
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-09-24 12:59:01 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,17 @@
 'use strict';
 
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function handleSubmit(event) {
+  event.preventDefault();
+
+  form.reset();
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
-});
-
-const form = document.getElementsByTagName('form');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  event.target.reset();
 });
