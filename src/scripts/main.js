32c60a1d0c31e08930abacd5967725dcef1beb6c
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-10-26 16:03:08 +0300

    museum_2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 'use strict';
 
 window.addEventListener('hashchange', () => {
@@ -7,3 +8,21 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('visit__visible');
+    } else {
+      change.target.classList.remove('visit__visible');
+    }
+  });
+}
+
+const options = { threshold: [0.1] };
+const observer = new IntersectionObserver(onEntry, options);
+const elements = document.querySelectorAll('.visit__photo');
+
+for (const elm of elements) {
+  observer.observe(elm);
+}
