16e74eb2108b81662cfcca1eed20e9a90f0db422
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2022-12-07 16:38:26 +0200

    Event listener for blocking reload on form submit was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -57,3 +57,10 @@ window.addEventListener('click', (e) => {
     };
   };
 });
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
