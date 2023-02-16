16b208f806c9946838f77b083e4e14f86d597371
Andrii Dudar <andrij.dudar@gmail.com>
2023-02-16 23:10:24 +0200

    ###

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,9 +13,10 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
     }
+    // else {
+    //   entry.target.classList.remove('show');
+    // }
   });
 });
 
