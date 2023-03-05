00d45844305c8ee9debbe73387fafbd9b1ba225f
Nazar Vorona <voron1765@gmail.com>
2023-03-05 04:46:56 +0200

    Fix  links and image hover

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.follow__form');
+const inputs = document.querySelectorAll('.follow__input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+});
