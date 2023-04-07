8d1d256f7b0ec0e2a8d1dcddf6972d27e48494e9
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-04-07 18:42:09 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const firstEventItem = document.querySelector('#firstEvent');
+const secondEventItem = document.querySelector('#secondEvent');
+const form = document.querySelector('.digest__from');
+
+const handleEventItemSlide = (eventItem) => {
+  const triggerPoint = eventItem.offsetTop
+   + eventItem.offsetHeight - window.innerHeight;
+
+  if (window.pageYOffset > triggerPoint) {
+    eventItem.style.transform = 'translateX(0px)';
+    eventItem.style.opacity = 1;
+  }
+};
+
+window.addEventListener('scroll', () => {
+  handleEventItemSlide(firstEventItem);
+});
+
+window.addEventListener('scroll', () => {
+  handleEventItemSlide(secondEventItem);
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formElements = [...form.elements];
+
+  formElements.forEach(element => {
+    if (element.type === 'email') {
+      element.value = '';
+    }
+  });
+});
