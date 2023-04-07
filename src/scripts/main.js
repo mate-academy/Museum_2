b45857508668583675d56bdeb0fa00b4ca1c5ed0
Kyrychenko Illia <i.kirichenko160804@gmail.com>
2023-04-07 20:32:25 +0300

    Fixed social and logo links & Disabled scroll while menu is open

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -33,3 +33,11 @@ form.addEventListener('submit', (e) => {
     }
   });
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
