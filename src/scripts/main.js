86af095a4ea35413204be41e8a82af9cc2d6ec4b
Eugene Kulish <genekulish13@gmail.com>
2023-04-22 19:51:53 +0300

    add scripts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('.subscription__form');
+
+function stopRefreshing(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', stopRefreshing);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
