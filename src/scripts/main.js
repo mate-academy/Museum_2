abd3587c28eca9924d00fd044e63e44fd64db4d5
Anna Peretiatko <tobossanna@gmail.com>
2023-04-25 14:34:28 +0200

    Museum landing v.2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,36 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const arrow = document.querySelector('.footer__arrow a');
+
+  arrow.addEventListener('click', event => {
+    event.preventDefault();
+
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+});
+
 window.addEventListener('hashchange', () => {
+  const pageBody = document.querySelector('.page__body');
+  const page = document.querySelector('.page');
+
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+    pageBody.classList.add('page__body--with-menu');
+    page.style.overflowY = 'hidden';
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    pageBody.classList.remove('page__body--with-menu');
+    page.style.overflowY = 'auto';
   }
 });
 
-const arrow = document.querySelector('.footer__arrow a');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
 
-arrow.addEventListener('click', event => {
-  event.preventDefault();
-
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    form.reset();
   });
 });
