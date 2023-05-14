4346ecdc2c5d7205904c9e8b17e3981a834c2925
Denis Kononenko <deniskononenko88@gmail.com>
2023-05-14 15:00:17 +0300

    add opening/closing menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const menuOpener = document.querySelector('.header__menu-opener');
+const menuCross = document.querySelector('.menu__cross');
+const menuLogo = document.querySelector('.menu__logo');
+const menuLinks = document.querySelectorAll('.menu__link');
+const menuButton = document.querySelector('.menu__button');
+
+const openMenu = () => menu.classList.add('menu--open');
+const closeMenu = () => menu.classList.remove('menu--open');
+
+menuOpener.addEventListener('click', openMenu);
+menuCross.addEventListener('click', closeMenu);
+menuLogo.addEventListener('click', closeMenu);
+menuButton.addEventListener('click', closeMenu);
+
+menuLinks.forEach(item => item.addEventListener('click', closeMenu));
