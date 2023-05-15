8ad2195a25d44474c861d778dfd4466a2d8dd7df
Mykhailiuk Viktor <117285070+Viktor535x@users.noreply.github.com>
2023-05-15 20:34:58 +0300

    test12

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,11 @@ function submit(event) {
 }
 
 form.addEventListener('submit', submit);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
