3b18fa726e789b855865a31be3ae51671eb8f8f5
Denis Kononenko <deniskononenko88@gmail.com>
2023-05-17 15:18:29 +0300

    add locale

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const header = document.querySelector('.header');
 const menu = document.querySelector('.menu');
 const menuOpener = document.querySelector('.header__menu-opener');
 const menuCross = document.querySelector('.menu__cross');
@@ -7,9 +8,25 @@ const menuLogo = document.querySelector('.menu__logo');
 const menuLinks = document.querySelectorAll('.menu__link');
 const menuButton = document.querySelector('.menu .button');
 const form = document.querySelector('.subscribe__form');
+const headerCurrentLocale = header.querySelector('.locale__current');
+const headerLocalesList = header.querySelector('.locale__list');
+const headerLocalesListItems = header.querySelectorAll('.locale__item');
+const menuCurrentLocale = menu.querySelector('.locale__current');
+const menuLocalesList = menu.querySelector('.locale__list');
+const menuLocalesListItems = menu.querySelectorAll('.locale__item');
 
 const openMenu = () => menu.classList.add('menu--open');
 const closeMenu = () => menu.classList.remove('menu--open');
+
+const openLocalesList = (localeList, currentLocale) => {
+  localeList.classList.toggle('locale__list--open');
+  currentLocale.classList.toggle('locale__current--opened-list');
+};
+
+const localeChoosingHandler = (event, localeList, currentLocale) => {
+  currentLocale.innerText = event.target.innerText;
+  localeList.classList.remove('locale__list--open');
+};
 
 menuOpener.addEventListener('click', openMenu);
 menuCross.addEventListener('click', closeMenu);
@@ -21,4 +38,24 @@ menuLinks.forEach(item => item.addEventListener('click', closeMenu));
 form.addEventListener('click', (e) => {
   e.preventDefault();
   form.reset();
+});
+
+headerCurrentLocale.addEventListener('click', () => {
+  openLocalesList(headerLocalesList, headerCurrentLocale);
+});
+
+menuCurrentLocale.addEventListener('click', () => {
+  openLocalesList(menuLocalesList, menuCurrentLocale);
+});
+
+headerLocalesListItems.forEach(item => {
+  item.addEventListener('click', event => {
+    localeChoosingHandler(event, headerLocalesList, headerCurrentLocale);
+  });
+});
+
+menuLocalesListItems.forEach(item => {
+  item.addEventListener('click', event => {
+    localeChoosingHandler(event, menuLocalesList, menuCurrentLocale);
+  });
 });
