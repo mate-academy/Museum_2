4574b03109bfce4d6d7cfb6d8643012640da24e3
Maryna Kyrychynska <m_kirichok@ukr.net>
2023-05-31 18:02:37 +0300

    museum-2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.digest__form');
+
+function submitForm(event) {
+  event.preventDefault();
+}
+form.addEventListener('submit', submitForm);
