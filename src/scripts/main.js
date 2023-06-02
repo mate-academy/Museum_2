699901e93fc2a0c56ddaabbfaf336f128d1b2e45
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2023-06-02 22:58:38 +0300

    version 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ frm.addEventListener('submit', (event) => {
   event.preventDefault();
   frm.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
