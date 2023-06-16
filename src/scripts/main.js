daa981f0a8270e9401bc65f415619efd93e5accf
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-06-16 14:14:50 +0300

    Need to add big photos and fix container with grid

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const body = document.getElementById('home');
+const header = document.querySelector('.header');
+const navLinkHeader = header.querySelectorAll('.nav__link');
+const headerButton = header.querySelector('.button');
+const form = document.querySelector('.subscribe__form');
+const footer = document.querySelector('.footer');
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('header__burger-menu--active');
+  menu.classList.toggle('menu--active');
+  headerButton.classList.toggle('button--menu-active');
+  body.classList.toggle('block-scroll');
+});
+
+navLinkHeader.forEach((link) => {
+  link.addEventListener('click', () => {
+    burgerMenu.classList.toggle('header__burger-menu--active');
+    menu.classList.toggle('menu--active');
+    headerButton.classList.toggle('button--menu-active');
+    body.classList.toggle('block-scroll');
+  });
+});
+
+headerButton.addEventListener('click', () => {
+  footer.scrollIntoView({ behavior: 'smooth' });
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
