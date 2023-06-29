8f55000088f21585b6c5d2ba2a50d12ae869824f
Andrii Khomiak <andr.hamster@gmail.com>
2023-06-30 01:15:47 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const dropdown1 = document.querySelector('.dropdown');
+const trigger1 = document.querySelector('.dropdown__trigger');
+
+const dropdown2 = document.querySelectorAll('.dropdown')[1];
+const trigger2 = document.querySelectorAll('.dropdown__trigger')[1];
+
+trigger1.addEventListener('click', () => {
+  dropdown1.classList.toggle('dropdown--active');
+});
+
+trigger2.addEventListener('click', () => {
+  dropdown2.classList.toggle('dropdown--active');
+});
