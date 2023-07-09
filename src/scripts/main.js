ff7ddb3849bb76ade57188d96b740f919a25d1aa
Kristina Bekher <bekherkr@gmail.com>
2023-07-09 20:47:12 +0200

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function submitForm(event) {
+  event.preventDefault();
+  document.querySelector('#form').reset();
+  window.location.hash = '#home';
+}
+
+document.querySelector('#form').addEventListener('submit', submitForm);
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(el => {
+  const trigger = el.querySelector('.dropdown__trigger');
+
+  trigger.addEventListener('click', (event) => {
+    event.preventDefault();
+    el.classList.toggle('dropdown--active');
+  });
+});
