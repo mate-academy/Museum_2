74438fc871d28594fe1f7ef4ebaf94ebcf1c6f9a
Dima Astapenko <bonum_taurus@ukr.net>
2023-07-19 13:18:03 +0300

    made landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const formDate = document.querySelector('.subscribe__form');
+
+formDate.addEventListener('submit', (event) => {
+  event.preventDefault();
+  formDate.reset();
+});
