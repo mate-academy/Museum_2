a599b646939a95f84296e0d0ac91d71174fe9f7e
Mykhailo Hubko <mixelios@gmail.com>
2023-08-21 18:24:57 +0200

    NAMU page is done

--- checklist.md
+++ checklist.md
@@ -3,14 +3,14 @@
 
 ## Tech review
 
-1. Add [favicon](http://joxi.ru/l2ZoZGJulwX3yA)
-2. Add a smooth scroll for the whole page (`scroll-behavior: smooth;`)
-3. All interactive elements(*links, buttons, pictures, icons*) ***should have a hover effect and cursor pointer***. User must intuitively understand that he can interact with them
-4. Pictures in the blocks "Exhibitions", "Events", "News" should be ***animated*** and somehow move on hover or when scrolling down the page
-5. Make sure when you click on logo there is no 404 error (Use `href="#"` in `anchor tag`)
-6. When you try to send the form there is no 405 error and the form is automatically cleared after submit and is scrolled to the top of the page or the page is reloaded
-7. The form shouldn’t submit empty
-8. After autocomplete the form, change the [default styles](https://user-images.githubusercontent.com/52370890/146189569-ff5e5387-d2bc-4781-ab8d-fd507582512e.png). Read more about [changing autocomplete styles](https://css-tricks.com/snippets/css/change-autocomplete-styles-webkit-browsers/)
+1. Add [favicon](http://joxi.ru/l2ZoZGJulwX3yA) <!-- + -->
+2. Add a smooth scroll for the whole page (`scroll-behavior: smooth;`) <!-- + -->
+3. All interactive elements(*links, buttons, pictures, icons*) ***should have a hover effect and cursor pointer***. User must intuitively understand that he can interact with them <!-- + -->
+4. Pictures in the blocks "Exhibitions", "Events", "News" should be ***animated*** and somehow move on hover or when scrolling down the page <!-- + -->
+5. Make sure when you click on logo there is no 404 error (Use `href="#"` in `anchor tag`) <!-- + -->
+6. When you try to send the form there is no 405 error and the form is automatically cleared after submit and is scrolled to the top of the page or the page is reloaded <!-- + -->
+7. The form shouldn’t submit empty <!-- + -->
+8. After autocomplete the form, change the [default styles](https://user-images.githubusercontent.com/52370890/146189569-ff5e5387-d2bc-4781-ab8d-fd507582512e.png). Read more about [changing autocomplete styles](https://css-tricks.com/snippets/css/change-autocomplete-styles-webkit-browsers/) <!-- + -->
 9. OPTIONAL: After everything is done, you can add a slider to the "News" block (for mobile version).
 
 ## HR review
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,107 @@
 'use strict';
+
+const isMobile = {
+  Android: () => {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: () => {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: () => {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: () => {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: () => {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: () => {
+    return (
+      isMobile.Android()
+      || isMobile.BlackBerry()
+      || isMobile.iOS()
+      || isMobile.Opera()
+      || isMobile.Windows()
+    );
+  },
+};
+
+const headerContent = document.querySelector('.header__content');
+const headerImage = document.querySelector('.header__main-image');
+const navigationContent = document.querySelector('.navigation__container');
+const menuImage = document.querySelector('.navigation__background-image');
+const windowHeight = window.innerHeight;
+const mainMenu = document.querySelector('.top-navigation');
+const menuOpener = document.querySelector('.top-action__menu-icon--opener');
+const menuCloser = document.querySelector('.top-action__menu-icon--closer');
+const headerLinks = document.querySelectorAll('.navigation__menu-link');
+const body = document.querySelector('.page__body');
+const scrolWidth = window.innerWidth - document.body.clientWidth;
+const mediaQueryOnDesktop = window.matchMedia('(min-width: 1280px)');
+const form = document.querySelector('.subsription__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+});
+
+if (!mediaQueryOnDesktop.matches) {
+  if (headerImage) {
+    headerImage.style.height
+      = windowHeight - headerContent.offsetHeight - 40 + 'px';
+  }
+
+  if (menuImage) {
+    menuImage.style.height
+      = windowHeight - navigationContent.offsetHeight - 15 + 'px';
+  }
+}
+
+if (menuOpener) {
+  menuOpener.addEventListener('click', (e) => {
+    menuOpener.classList.toggle('_active');
+    body.classList.toggle('_lock');
+    mainMenu.classList.toggle('_open');
+
+    if (!isMobile.any()) {
+      if (body.classList.contains('_lock')) {
+        body.style.paddingRight = scrolWidth + 'px';
+      } else {
+        body.style.paddingRight = 0;
+      }
+    }
+  });
+}
+
+if (menuCloser) {
+  menuCloser.addEventListener('click', (e) => {
+    body.classList.toggle('_lock');
+    mainMenu.classList.toggle('_open');
+
+    if (!isMobile.any()) {
+      if (body.classList.contains('_lock')) {
+        body.style.paddingRight = scrolWidth + 'px';
+      } else {
+        body.style.paddingRight = 0;
+      }
+    }
+  });
+}
+
+if (menuCloser) {
+  for (let i = 0; i < headerLinks.length; i++) {
+    headerLinks[i].addEventListener('click', (e) => {
+      body.classList.toggle('_lock');
+      mainMenu.classList.toggle('_open');
+
+      if (!isMobile.any()) {
+        if (body.classList.contains('_lock')) {
+          body.style.paddingRight = scrolWidth + 'px';
+        } else {
+          body.style.paddingRight = 0;
+        }
+      }
+    });
+  }
+}
