429b68afcea8874091d9ccea8283620ae22d3d1c
petrushinsky-kirill <petrushinsky@gmail.com>
2023-10-22 19:38:06 +0300

    implemented all blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const dropdownHeader = document.querySelector('header .dropdown');
+const triggerHeader = dropdownHeader.querySelector('.dropdown__trigger');
+const contentHeader = dropdownHeader.querySelector('.dropdown__items');
+
+triggerHeader.addEventListener('click', () => {
+  if (dropdownHeader.classList.contains('dropdown--active')) {
+    contentHeader.style.maxHeight = '30';
+    dropdownHeader.classList.remove('dropdown--active');
+  } else {
+    contentHeader.style.maxHeight = `60`;
+    dropdownHeader.classList.add('dropdown--active');
+  }
+});
+
+const dropdownMenu = document.querySelector('aside .dropdown');
+const triggerMenu = dropdownMenu.querySelector('.dropdown__trigger');
+const contentMenu = dropdownMenu.querySelector('.dropdown__items');
+
+triggerMenu.addEventListener('click', () => {
+  if (dropdownMenu.classList.contains('dropdown--active')) {
+    contentMenu.style.maxHeight = '30';
+    dropdownMenu.classList.remove('dropdown--active');
+  } else {
+    contentMenu.style.maxHeight = `60`;
+    dropdownMenu.classList.add('dropdown--active');
+  }
+});
+
+const form = document.querySelector('.subscribe__form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  form.reset();
+});
