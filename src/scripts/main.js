000b46096995803798b57966b3101e9f93de7505
petrushinsky-kirill <petrushinsky@gmail.com>
2023-10-23 19:00:14 +0300

    implemented all blocks UPD2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -42,3 +42,46 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+
+const animItems = document.querySelectorAll('.card__photo');
+
+window.addEventListener('scroll', animOnScroll);
+
+function animOnScroll() {
+  if (animItems.length > 0) {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      }
+      // } else {
+      //   animItem.classList.remove('_active');
+      // }
+    }
+  }
+}
+
+function offset(el) {
+  const rect = el.getBoundingClientRect();
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  return {
+    top: rect.top + scrollTop, left: rect.left + scrollLeft,
+  };
+}
+
+setTimeout(() => {
+  animOnScroll();
+}, 300);
