06500d3be163ed803b414616c51782f08ff91390
petrushinsky-kirill <petrushinsky@gmail.com>
2023-10-23 12:06:24 +0300

    implemented all blocks UPD

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,3 +34,11 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
