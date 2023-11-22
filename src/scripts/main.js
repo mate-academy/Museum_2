4e45d2f6a51494f866e4c0e9298502368377fb29
Maksym_Kos <maxkos18@gmail.com>
2023-11-22 13:19:54 +0200

    final

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,15 +2,16 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with--menu');
+    document.body.classList.add('page__body--with-menu');
   } else {
-    document.body.classList.remove('page__body--with--menu');
+    document.body.classList.remove('page__body--with-menu');
   }
 });
 
-const form = document.getElementsByClassName('form')[0];
+const pageReload = document.querySelector('#form-reload');
 
-form.addEventListener('submit', (event) => {
-  form.reset();
+pageReload.addEventListener('submit', function formReload(event) {
   event.preventDefault();
+
+  pageReload.reset();
 });
