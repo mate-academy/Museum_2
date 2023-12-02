acc52c9040dcd3d6be93d369358d41c637ecaadd
Denys209818 <deniskravcuk63@gmail.com>
2023-12-03 01:55:04 +0200

    Load full site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.getElementById('main-form').addEventListener('submit',
+  function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const input = document.querySelector('.main__form-input');
+
+    input.value = '';
+  });
