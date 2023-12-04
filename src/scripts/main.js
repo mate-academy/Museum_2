d114a77c6037ae2d762e792576c685ffc425c20c
Dominika Karmel <dkarmel@op.pl>
2023-12-04 08:52:20 +0100

    footer and upward done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// upward
+const scroll = document.querySelector('.upward');
+
+window.addEventListener('scroll', () => {
+  scroll.classList.toggle('upward--active', window.scrollY > 500);
+});
+
+scroll.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
