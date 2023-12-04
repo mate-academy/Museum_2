57cb713bd338bbde8f777adcb2de76b5c5dcd66e
Dominika Karmel <dkarmel@op.pl>
2023-12-04 11:59:27 +0100

    menu for mobile ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,23 @@ window.addEventListener('scroll', () => {
 scroll.addEventListener('click', () => {
   window.scrollTo(0, 0);
 });
+
+// menu
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuIcon = document.querySelector('.menuIcon');
+  const closeIcon = document.querySelector('.closeIcon');
+  const menu = document.querySelector('.menu');
+
+  menuIcon.addEventListener('click', function() {
+    menu.style.display = 'flex';
+    menuIcon.style.display = 'none';
+    closeIcon.style.display = 'block';
+  });
+
+  closeIcon.addEventListener('click', function() {
+    menu.style.display = 'none';
+    menuIcon.style.display = 'block';
+    closeIcon.style.display = 'none';
+  });
+});
