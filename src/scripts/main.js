8797776e83ffe95f2072015a4f3ceb6580db7dab
Olha Shvets <lv160888soi@gmail.com>
2024-01-24 10:16:05 +0200

    add a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const scrollToTopButton = document.querySelector('.footer__scroll-up');
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
