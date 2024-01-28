e66203282c144ddfdc0aa44a3d9f4cf8639783f2
nmaafk0510@gmail.com <nmaafk0510@gmail.com>
2024-01-28 15:00:38 +0200

    add fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,23 @@
 document.getElementById('btn__form').onclick = function(e) {
   document.getElementById('input__form').value = '';
 };
+
+// const menu = document.querySelector('.menu')
+// const openButton = document.querySelector('.topline__menu')
+
+// openButton.addEventListener('click', function() {
+//   menu.style.display = 'block';
+//   document.body.classList.add('menu-open');
+// });
+
+// const closeFunc = function () {
+//   menu.style.display = 'none';
+//   document.body.classList.remove('menu-open');
+// }
+
+// const closeButtonAnother = document.querySelectorAll('.navigation__point')
+// const closeButton = document.querySelector('.topline_aside__cross');
+
+// closeButton.addEventListener('click', closeFunc);
+// closeButtonAnother.forEach((node)
+//= > node.addEventListener('click', closeFunc));
