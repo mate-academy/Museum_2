b2acc1f83adb3c6ccce0e844149b7e2267e3debc
Tetiana Bukhenko <tania04bukhenko@gmail.com>
2024-02-18 22:29:01 +0200

    updated task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const buttons = document.getElementsByClassName('button');
+const subscribeButton = document.getElementById('subscribe__button');
+
+const clickHandler = function(event) {
+  // eslint-disable-next-line no-undef
+  alert('This button works 😉');
+};
+
+subscribeButton.addEventListener('click', (event) => {
+  // eslint-disable-next-line no-undef
+  alert('You have successfully subscribed to our newsletter 😉');
+});
+
+for (const button of buttons) {
+  button.addEventListener('click', clickHandler);
+}
