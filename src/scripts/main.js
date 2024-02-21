06f50c2683c8457d15e6171cbadadcfc5edb2286
TetianaBukhenko <tania04bukhenko@gmail.com>
2024-02-21 13:41:50 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,32 @@ subscribeButton.addEventListener('click', (event) => {
 for (const button of buttons) {
   button.addEventListener('click', clickHandler);
 }
+
+const showMenu = () => {
+  document.getElementById('menu').classList.add('show');
+
+  const body = document.body;
+
+  body.style.height = '100vh';
+  body.style.overflowY = 'hidden';
+};
+
+const closeMenu = () => {
+  const body = document.body;
+  const scrollY = body.style.top;
+
+  body.style.position = '';
+  body.style.top = '';
+  body.style.height = '';
+  body.style.overflowY = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  document.getElementById('menu').classList.remove('show');
+};
+
+const menuIcon = document.getElementById('menu-icon');
+
+menuIcon.addEventListener('click', showMenu);
+
+const menuClose = document.getElementById('menu-close');
+
+menuClose.addEventListener('click', closeMenu);
