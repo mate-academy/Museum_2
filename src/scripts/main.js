d7c458ec9fc317d23c20d62bb6f5c1b83f301b88
Oleksandr Robenko <oleksandr.robenko@gmail.com>
2024-03-19 15:46:24 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function disable() {
+  document.querySelector('.page__body').classList.add('disable-scroll');
+}
+
+function enable() {
+  document.querySelector('.page__body').classList.remove('disable-scroll');
+}
+
+document.querySelector('#prevent').addEventListener('click', disable);
+document.querySelector('#allow').addEventListener('click', enable);
