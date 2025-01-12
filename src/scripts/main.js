ce2cda87495fd4b0c3003f235773d0fcf48f9f12
beliyandr <beliyandr@gmail.com>
2025-01-12 23:05:21 +0200

    &

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const topBar = document.querySelector('.header__top');
+const burger = topBar.querySelector('.header__top-button');
+const menu = document.querySelector('.menu');
+const close = menu.querySelector('.menu__top-button')
+
+burger.addEventListener('click', (e) => {
+  menu.classList.add('active')
+
+})
+
+close.addEventListener('click', (e) => {
+  menu.classList.remove('active')
+
+})
