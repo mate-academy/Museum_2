93ab6bd839b1b200366b931fa444eb812560a491
beliyandr <beliyandr@gmail.com>
2025-01-13 23:07:05 +0200

    popup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const topBar = document.querySelector('.header__top');
-const burger = topBar.querySelector('.header__top-button');
+const menuBurger = topBar.querySelector('.header__top-button');
 const menu = document.querySelector('.menu');
-const close = menu.querySelector('.menu__top-button')
+const menuLinks = document.querySelectorAll('.navigation__item')
+const menuClose = menu.querySelector('.top-bar__button')
+const body = document.querySelector('body');
+const popup = document.querySelector('.popup');
 
-burger.addEventListener('click', (e) => {
-  menu.classList.add('active')
+function activeMenu() {
+  menu.classList.add('active');
+  body.style.overflowY = 'hidden';
 
-})
+  menuLinks.forEach((item) => {
 
-close.addEventListener('click', (e) => {
-  menu.classList.remove('active')
+    item.addEventListener('click', closeMenu);
+  });
+}
 
-})
+function closeMenu() {
+  menu.classList.remove('active');
+  body.style.overflowY = 'auto';
+
+}
+
+menuBurger.addEventListener('click', activeMenu)
+menuClose.addEventListener('click', closeMenu)
