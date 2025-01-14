b441f57200d44c490ffc61415e154a880d9783fc
beliyandr <beliyandr@gmail.com>
2025-01-14 19:34:23 +0200

    1-version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,11 @@ const menu = document.querySelector('.menu');
 const menuLinks = document.querySelectorAll('.navigation__item')
 const menuClose = menu.querySelector('.top-bar__button')
 const body = document.querySelector('body');
+
+const from = document.querySelector('.digest__form')
 const popup = document.querySelector('.popup');
+const input = from.querySelector('.digest__input')
+const digestError = from.querySelector('.digest__error');
 
 function activeMenu() {
   menu.classList.add('active');
@@ -24,5 +28,24 @@ function closeMenu() {
 
 }
 
+function subscribe(event) {
+  event.preventDefault();
+  if (!input.value) {
+    digestError.style.opacity = 1;
+    const timeout = setTimeout(() => {
+      digestError.style.opacity = 0;
+      clearTimeout(timeout);
+    }, 2000);
+    return
+  }
+  input.value = '';
+  popup.classList.add('popup-active');
+  const timeout = setTimeout(() => {
+    popup.classList.remove('popup-active');
+    clearTimeout(timeout);
+  }, 2000);
+}
+
+from.addEventListener('submit', subscribe);
 menuBurger.addEventListener('click', activeMenu)
 menuClose.addEventListener('click', closeMenu)
