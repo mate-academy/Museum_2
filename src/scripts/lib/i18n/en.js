5598cfe2cf1133be564e27f708cd7f98a2c8c324
beliyandr <beliyandr@gmail.com>
2025-01-22 16:39:02 +0200

    i18

--- src/scripts/lib/i18n/en.js
+++ src/scripts/lib/i18n/en.js
@@ -3,11 +3,23 @@ export default {
   'data-button-buy': ' buy ticket',
 
   //header:
-  'data-header-top-data-start': "10 august",
-  'data-header-top-data-end': "10 november",
+  'data-header-top-data-start': '10 august',
+  'data-header-top-data-end': '10 november',
   'data-header-title': 'Art of the 19th - 20th centuries.',
-  'data-header-text': 'The contribution of Ukrainian artists to the world culture of the 19th and 20th centuries',
+  'data-header-text':
+    'The contribution of Ukrainian artists to the world culture of the 19th and 20th centuries',
 
   //menu:
+  'data-menu-info-sub': 'Розклад сьогодні: ',
+  'data-menu-info-sub': 'Адреса: ',
+  'data-menu-info-text': 'Київ, вул. М. Грушевського, 6',
+  'data-menu-exhibitions': 'Актуальні виставки',
+  'data-menu-events': 'Найближчі події',
+  'data-menu-news': 'Новини',
 
+  // popup
+  'data-popup__title': 'Ваша заявка прийнята',
+  'data-popup__text': 'Дуже дякую за підписку!',
+
+  //exhibitions
 };
--- src/scripts/lib/i18n/ua.js
+++ src/scripts/lib/i18n/ua.js
@@ -9,4 +9,18 @@ export default {
   'data-header-text': 'Внесок українських митців у світову культуру 19-20ст',
 
   //menu:
+  'data-menu-info-sub': 'Розклад сьогодні:',
+  'data-menu-info-sub': 'Адреса:',
+  'data-menu-info-text': 'Київ, вул. М. Грушевського, 6',
+  'data-menu-exhibitions': 'Актуальні виставки',
+  'data-menu-events': 'Найближчі події',
+  'data-menu-news': 'Новини',
+
+  // popup
+  'data-popup__title': 'Ваша заявка прийнята',
+  'data-popup__text': 'Дуже дякую за підписку!',
+
+  //exhibitions
+
+
 };
