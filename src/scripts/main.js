c698fcefe411aa7e6b37843cbb785c3abe013842
Daria <darialys2510@gmail.com>
2025-03-18 15:50:42 +0100

    delete class container

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+// const langSwitcher = document.querySelector('.langSwitcher');
+// const trigger = langSwitcher.querySelector('.langSwitcher__trigger');
+
+// trigger.addEventListener('contextmenu', (event) => {
+//   event.preventDefault();
+
+//   // langSwitcher.classList.toggle('langSwitcher--active');
+
+//   if (langSwitcher.classList.contains('langSwitcher--active')) {
+//     langSwitcher.classList.remove('langSwitcher--active');
+//   } else {
+//     langSwitcher.classList.add('langSwitcher--active');
+//   }
+// });
