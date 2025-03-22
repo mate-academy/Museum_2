cd0ab30233fc83bb291e013490ab4b2b23d01ddc
Taras <luchakt2@gmail.com>
2025-03-22 12:37:13 +0200

    Вимкнено скрол під час відкритого меню та виправлено посилання на адресу

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,39 +15,60 @@ document.addEventListener("DOMContentLoaded", () => {
       localStorage.setItem("language", currentLang);
     });
   });
+
+  const form = document.getElementById("subscribeForm");
+  if (form) {
+    form.addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const emailInput = document.getElementById("subscribeInput");
+      const emailError = document.getElementById("email-error");
+      const successMessage = document.getElementById("successMessage");
+
+      if (!emailInput || !emailError || !successMessage) return;
+
+      let isValid = true;
+      const emailPattern = /^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/;
+      const emailValue = emailInput.value.trim().replace(/\s+/g, "");
+
+      if (!emailPattern.test(emailValue)) {
+        emailError.textContent = "Please enter a valid email without spaces.";
+        emailError.style.display = "block";
+        emailInput.classList.add("error");
+        isValid = false;
+      } else {
+        emailError.style.display = "none";
+        emailInput.classList.remove("error");
+      }
+
+      if (isValid) {
+        successMessage.classList.add("show");
+        form.reset();
+
+        setTimeout(() => {
+          successMessage.classList.remove("show");
+       }, 3000);
+      }
+    });
+  }
+
+  const menuToggle = document.getElementById("menuToggle");
+  const menu = document.getElementById("menu");
+  const body = document.body;
+  const menuLinks = document.querySelectorAll(".menu a[href^='#']")
+
+  if (menuToggle && menu) {
+    menuToggle.addEventListener('click', (event) => {
+      event.preventDefault();
+      menu.classList.toggle("menu-open");
+      body.classList.toggle("no-scroll");
+    });
+  }
+
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("menu-open");
+      body.classList.remove("no-scroll");
+    })
+  });
 });
-
-document.getElementById("subscribeForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const form = event.target;
-  const emailInput = document.getElementById("subscribeInput");
-  const emailError = document.getElementById("email-error");
-  const successMassage = document.getElementById("successMessage");
-
-  let isValid = true;
-
-  const emailPattern = /^[a-zA-Z0-9.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/;
-  const emailValue = emailInput.value.trim().replace(/\s+/g, "");
-
-  if (!emailPattern.test(emailValue)) {
-    emailError.textContent = "Please enter a valid email without spaces.";
-    emailError.style.display = "block";
-    emailInput.classList.add("error");
-    isValid = false;
-  } else {
-    emailError.style.display = "none";
-    emailInput.classList.remove("error");
-  }
-
-  if (isValid) {
-    successMassage.classList.add("show");
-    form.reset();
-  }
-
-  setTimeout(() => {
-    successMassage.classList.remove("show");
-  }, 3000);
-})
-
-
