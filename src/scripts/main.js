4a04f8fafdf573d670e89067370266e901b54bc5
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-04-20 12:49:36 +0200

    done code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const form = document.getElementById('form');
+const input = document.getElementById('form_input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  input.value = '';
+  input.classList.add("subscribe__input--submitted");
+
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
