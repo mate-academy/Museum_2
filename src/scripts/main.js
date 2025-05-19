aa8d02f705c03f3386b60c3723f1f648540ccf09
diachkinainna <diachkinainna@gmail.com>
2025-05-19 09:59:12 +0300

    fixed position background-image and added target blank to links

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,18 +4,22 @@ const body = document.querySelector('.page__body');
 const open = document.querySelector('.icon--menu');
 const close = document.querySelector('.icon--close');
 const menu = document.querySelector('.menu');
+const scrollUp = document.querySelector('.page__scroll-up');
 
 open.addEventListener('click', () => {
   body.classList.add('no-scroll');
+  scrollUp.classList.add('hidden');
 })
 
 close.addEventListener('click', () => {
   body.classList.remove('no-scroll');
+  scrollUp.classList.remove('hidden');
 })
 
 menu.addEventListener('click', (event) => {
   if (event.target.tagName === 'A') {
     body.classList.remove('no-scroll');
+    scrollUp.classList.remove('hidden');
   }
 })
 
