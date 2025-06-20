818ef979bf1b497041ea19c2af15d4e6a2f13094
Lirik <melnicukkiril2002@gmail.com>
2025-06-20 21:16:21 +0300

    complete main and footer sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const scrollBtn = document.querySelector('#top');
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
